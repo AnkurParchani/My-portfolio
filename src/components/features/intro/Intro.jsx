fd4c9554858ed6dec442d61c926383bda80a329d
Ankur Parchani <ankurparchani@gmail.com>
2023-08-09 17:52:38 +0530

    fix: greeting and logo

--- src/components/features/intro/Intro.jsx
+++ src/components/features/intro/Intro.jsx
@@ -3,13 +3,6 @@ import Headline from "../../ui/Headline";
 import Section from "../../ui/Section";
 
 export default function Intro() {
-  let greet;
-  console.log(new Date().getHours());
-  const currentHours = new Date().getHours();
-  if (currentHours > 0 && currentHours < 12) greet = "Good morning!";
-  if (currentHours > 12 && currentHours < 18) greet = "Good afternoon!";
-  if (currentHours > 18) greet = "Good evening!";
-
   return (
     <AnimateOnScroll sectionName="section-intro">
       <Headline line="This is me..." headlineClass="sec-intro--headline" />
@@ -26,7 +19,7 @@ export default function Intro() {
             alt="my-image"
           />
           <h2 className="text-5xl font-semibold tracking-wider text-[#d9f7dc] sm:text-6xl lg:text-7xl">
-            {greet}
+            Hey there!
           </h2>
           <p className=" text-3xl font-normal italic leading-relaxed tracking-wider text-[#d9f7dc] sm:text-4xl sm:leading-relaxed md:leading-relaxed lg:text-5xl lg:leading-relaxed">
             I am a Fullstack Developer proficient in crafting sophisticated
--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div className="text-2xl font-medium italic tracking-wider text-[#99ffdb]  sm:text-3xl">
-      <a href="#intro">ankurparchani</a>
+      <a href="#intro">Ankur Parchani</a>
     </div>
   );
 }
