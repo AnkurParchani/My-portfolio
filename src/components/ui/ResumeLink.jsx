03572ae3bc689260432b884768a57121c46846aa
Ankur Parchani <ankurparchani@gmail.com>
2024-02-22 15:07:02 +0530

    update: resume link to google drive

--- src/components/ui/ResumeLink.jsx
+++ src/components/ui/ResumeLink.jsx
@@ -2,8 +2,9 @@ export default function ResumeLink() {
   return (
     <a
       target="_blank"
+      rel="noreferrer"
       className="rounded-md bg-[#24c58d] px-2 py-1 text-center text-[#e7e7e7] duration-200 hover:bg-gray-300 hover:text-gray-600 md:px-6 md:py-0"
-      href="/My-portfolio/Ankur's%20Resume.pdf"
+      href="https://drive.google.com/file/d/1e5WVaRYKcQVVMGwQ7LUoBM-9O3np2ndi/view?usp=sharing"
     >
       Resume
     </a>
