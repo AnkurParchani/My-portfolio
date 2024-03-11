f6a7fa768f4f5ded4b1a0e2931e85d9e9286020c
Ankur Parchani <ankurparchani@gmail.com>
2024-03-11 08:14:31 +0530

    modify: resume

--- src/components/ui/ResumeLink.jsx
+++ src/components/ui/ResumeLink.jsx
@@ -4,7 +4,7 @@ export default function ResumeLink() {
       target="_blank"
       rel="noreferrer"
       className="rounded-md bg-[#24c58d] px-2 py-1 text-center text-[#e7e7e7] duration-200 hover:bg-gray-300 hover:text-gray-600 md:px-6 md:py-0"
-      href="https://drive.google.com/file/d/1e5WVaRYKcQVVMGwQ7LUoBM-9O3np2ndi/view?usp=sharing"
+      href="https://drive.google.com/file/d/1vyHMolwE3W-THCpyHTd6DWtrEZgEuwNQ/view?usp=sharing"
     >
       Resume
     </a>
